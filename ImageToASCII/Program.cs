42c29f9849f00df158e0f24b5ef09e7352394c81
trezortop92@gmail.com <trezortop92@gmail.com>
2023-07-07 02:23:43 +0500

    code refactoring

--- ImageToASCII/Program.cs
+++ ImageToASCII/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System.Drawing;
 using ImageToASCII;
 
-string imagePath = "C:\\Users\\User\\Desktop\\50236551.png";
+const int maxHeight = 170;
+const float offsetWidth = 2.5f;
+char[] chars = { ' ', '.', ':', '+', '*', 'o', 'x', '8', 'B', 'M', 'W', 'X', '&', '$', '%', '#', '@' };
+
+string imagePath = "C:\\Users\\User\\Desktop\\photo.jpg";
 // string imagePath = "C:\\Users\\User\\Desktop\\50236551 - Copy (2).png";
 
 using FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
 Bitmap bitmap = new Bitmap(fileStream);
 
-float aspectRatio = (float)bitmap.Width / bitmap.Height;
-int maxHeight = 170;
-int newWidth = (int)(maxHeight * aspectRatio * 2.5f);
-
-if (bitmap.Height > maxHeight || bitmap.Width > newWidth)
-    bitmap = new Bitmap(bitmap, new Size() { Width = newWidth, Height = maxHeight });
-
+ResizeBitmap(ref bitmap);
 bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
 bitmap.ToGrayScale();
+
+int step = 255 / chars.Length;
 
 for (int x = 0, width = bitmap.Width; x < width; x++)
 {
@@ -23,10 +23,19 @@ for (int x = 0, width = bitmap.Width; x < width; x++)
     {
         Color color = bitmap.GetPixel(x, y);
 
-        Console.Write(color.R > 162 ? 'O' : 'H');
+        Console.Write(chars[color.R / step]);
     }
 
     Console.WriteLine();
 }
 
 Console.ReadLine();
+
+void ResizeBitmap(ref Bitmap bitmap)
+{
+    float aspectRatio = (float)bitmap.Width / bitmap.Height;
+    int newWidth = (int)(maxHeight * aspectRatio * offsetWidth);
+
+    if (bitmap.Height > maxHeight || bitmap.Width > newWidth)
+        bitmap = new Bitmap(bitmap, new Size() { Width = newWidth, Height = maxHeight });
+}
